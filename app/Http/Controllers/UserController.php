c234a2496c4d10010592e4068c240cd619c92bdf
Marco <marco1102004@gmail.com>
2024-03-03 15:47:14 -0600

    merge data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,8 +8,6 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function index(){
-        return response()->json([
-            'Users' => User::all()
-        ]);
+        return User::all();
     }
 }
